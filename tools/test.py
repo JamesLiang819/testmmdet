6addc9923f64ccb17a777726caf1753a28a3c833
JamesLiang819 <jamesliang819@gmail.com>
2021-12-20 09:51:01 -0500

    test

--- tools/test.py
+++ tools/test.py
@@ -19,6 +19,8 @@ from mmdet.datasets import (build_dataloader, build_dataset,
 from mmdet.models import build_detector
 
 
+import tensorboard
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description='MMDet test (and eval) a model')
